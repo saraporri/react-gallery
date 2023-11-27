6ca2f744654fe8776115e1aa837a6b03747e640e
saraporri <saraporricino@yahoo.com>
2023-11-27 14:16:54 +0100

    Added SearchEngine component

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,32 +1,17 @@
-import React, { useState } from "react";
-import axios from "axios";
-
-export default function SearchEngine(props) {
-  let [keyword, setKeyword] = useState(props.defaultKeyword);
-  let [results, setResults] = useState(null);
-
-  function handleApiResponse(response) {
-    setResults(response.data);
-  }
-  function search() {
-    //https://picsum.photos/
-    let apiUrl = `https://picsum.photos/id/${keyword}v2/list&limit=7`;
-    axios.get(apiUrl).then(handleApiResponse);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    search();
-  }
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
-  }
-  <form onSubmit={handleSubmit}>
-    <input
-      type="search"
-      onChange={handleKeywordChange}
-      defaultValue={props.defaultKeyword}
-    />{" "}
-    <Results results={results} />
-  </form>;
+import React from "react";
+import "./SearchEngine.css";
+export default function SearchEngine() {
+  return (
+    <form>
+      <h1>Find Images</h1>
+      <div>
+        <input
+          type="search"
+          className="search"
+          placeholder="Search images..."
+        />
+        <button>Search</button>
+      </div>
+    </form>
+  );
 }
