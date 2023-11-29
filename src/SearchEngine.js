9954e7dbabdf47a63640a234ebace7d8cdccfe1d
saraporri <saraporricino@yahoo.com>
2023-11-29 17:50:58 +0100

    Updated components

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,56 +1,55 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Images from "./Images";
+import Photos from "./Photos";
 import "./SearchEngine.css";
 export default function SearchEngine() {
   let [keyword, setKeyword] = useState("");
-  let [images, setImages] = useState(null);
+  let [photos, setPhotos] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
   function handleGalleryResponse(response) {
-    setImages(response.data);
+    console.log(response.data);
+
+    setPhotos(response.data);
   }
+  function search() {    const apiKey = "8b07dt8e17fee9f438eo54a03e745179";
 
-  function search() {
-    const apiKey = "rVkc9-jh7sIiRdN12GWabdrJ4e__vva1eVy2HMVSp-E";
-    let apiUrl = `https://api.unsplash.com/search/collections?page=1&query=office&client_id=${apiKey}`;
-
-    axios.get(apiUrl).then(handleGalleryResponse);
+    let apiUrl=`https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}&per_page=6`;
+    axios.get(ApiUrl).then(handleGalleryResponse);
   }
-
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+    search();
+  }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  function handleButtonSearch() {
-    setKeyword("");
-    search();
-  }
+}
   function load() {
     setLoaded(true);
     search();
   }
 
-  if (loaded) {
-    return (
-      <div className="searchEngine">
-        <form>
-          <h1>Find Images</h1>
-          <div>
-            <input
-              type="search"
-              className="search"
-              placeholder="Search images..."
-              defaultValue={keyword}
-              onChange={handleKeywordChange}
-            />
-            <button onClick={handleButtonSearch}>Search</button>
-          </div>
-        </form>
-        <Images images={images} />
-      </div>
-    );
-  } else {
-    load();
-    return "Loading";
-  }
+
+if (loaded) {
+  return (
+    <div className="Search">
+      <h1>Find Images</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="search"
+          placeholder="Search images..."
+          value={keyword}
+          onChange={handleKeywordChange}
+        />
+      </form>
+        <Photos photos={photos} />
+    </div>
+  );
+} else {
+  load();
+  return "Loading";
+}
 }
