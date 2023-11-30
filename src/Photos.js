c8db4b4232ea4a5b14f57f0af20413c692bbf1d1
saraporri <saraporricino@yahoo.com>
2023-11-30 22:26:05 +0100

    Changed css

--- src/Photos.js
+++ src/Photos.js
@@ -5,13 +5,13 @@ export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="Photos">
-        <div>
+        <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div key={index}>
+              <div className="col-4" key={index}>
                 <img
-                  src={photo.src.landscape}
-                  className="img-fluid"
+                  src={photo.src.original}
+                  className="img-fluid image"
                   alt={photo.alt}
                 />
               </div>
